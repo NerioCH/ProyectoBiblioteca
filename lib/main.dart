c34bc4b8f84e69451fc72d40ae9ea18d87e1ff9d
NerioCH <huarcayanerio@gmail.com>
2023-01-09 17:37:18 -0500

    inicio de copia de datos

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:controldegastos/aplication/use_cases/frmPrincipal.dart';
+import 'package:controldegastos/aplication/use_cases/login/login.dart';
+import 'package:controldegastos/aplication/use_cases/registro/registroUsuarios.dart';
+import 'package:controldegastos/aplication/use_cases/services/authService.dart';
+import 'package:controldegastos/rutas.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<AuthService>(
+          create: (_) => AuthService(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        // home: Rutas(),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const Rutas(),
+          '/home': (context) => const frmPrincipal(),
+          '/login': (context) => login(),
+          '/registro': (context) => registrarUsuario(),
+        },
+      ),
+    );
+  }
+}
